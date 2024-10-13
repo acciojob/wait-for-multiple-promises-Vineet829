f5d7d8eda4652c5c6f92570e53764152316b5fc3
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-14 02:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
-const tab = document.querySelector("#output");
+const res = document.getElementById("output");
 
-const random = () => {
-	return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-};
+const promises = [
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
+  }),
+];
 
-tab.innerHTML = `
-<tr>
-<td>Loading...</td>
-<td>Loading...</td>
-</tr>
-`;
+async function callFns() {
+  const start = new Date();
 
-function promise1() {
-	return new Promise((resolve, reject) => {
-		let time = random();
-		setTimeout(() => {
-			resolve(["Promise1", time]);
-		}, time * 1000);
-	});
+  res.innerHTML += `
+            <tr id="loading">
+                <td colspan=2>Loading...</td>
+            </tr>
+          `;
+  await Promise.all(promises)
+    .then((results) => {
+      res.innerHTML = ``;
+    
+      results.forEach((e) => {
+        res.innerHTML += `
+            <tr>
+                <td>${e.name}</td>
+                <td>${e.time}</td>
+            </tr>
+          `;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  const end = new Date();
+  const timeInMillis = end - start;
+  res.innerHTML += `
+            <tr>
+                <td>Total</td>
+                <td>${timeInMillis / 1000}</td>
+            </tr>
+          `;
 }
-
-function promise2() {
-	return new Promise((resolve, reject) => {
-		let time = random();
-		setTimeout(() => {
-			resolve(["Promise2", time]);
-		}, time * 1000);
-	});
-}
-
-function promise3() {
-	return new Promise((resolve, reject) => {
-		let time = random();
-		setTimeout(() => {
-			resolve(["Promise3", time]);
-		}, time * 1000);
-	});
-}
-
-Promise.all([promise1(), promise2(), promise3()])
-.then(res => {
-	tab.innerHTML = `
-	<tr>
-	<td>${res[0][0]}</td>
-	<td>${res[0][1]}</td>
-	</tr>
-	<tr>
-	<td>${res[1][0]}</td>
-	<td>${res[1][1]}</td>
-	</tr>
-	<tr>
-	<td>${res[2][0]}</td>
-	<td>${res[2][1]}</td>
-	</tr>
-	`;
-});
-
-	
+callFns();
 
